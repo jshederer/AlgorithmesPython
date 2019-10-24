38dd9464b92869e2e783a51a3cd5423d4e50a62c
jshederer <55618151+jshederer@users.noreply.github.com>
2019-10-24 23:34:22 +0200

    0.2

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # AlgorithmesPython
  Algorithmes en Python pour les mathématiques
+
+TripletsPythagore.py:
+ Algorithme testé par rapport à l'algorithme basique avec un seuil de 1100
+ L'algorithme basique est optimisé en prenant en compte les règles suivantes:
+   * a < b < c donc a < seuil-2 et b < seuil-1
+   * lorsque l'on parcourt les a pour un a donné, on pose que ...
+   * lorsque l'on parcourt les b pour un a donné, on pose que le maximum possible pour b²=seuil²-a²
+   * lorsque l'on parcourt les C pour un a et un b donnés, on pose que l'on cherche autour de c²=a²+b² (on cherche autour en raison des limitations techniques des nombres décimaux codés informatiquement)
--- TripletsPythagore.py
+++ TripletsPythagore.py
@@ -1,18 +1,16 @@
 ﻿import math
 
-def liste_triplets_optimise(seuil):
+def liste_triplets_brut_smart_smart_smart(seuil):
     resultat = []
-    coef_a = (-1+math.sqrt(2*seuil**2-1))/(2*seuil)
-    seuil_a = math.ceil(seuil*coef_a)
+    seuil_a = min(math.ceil((-1+math.sqrt(2*seuil**2-1))/(2)),seuil-2)
     for a in range(1,seuil_a+1):
-        coef_b = math.sqrt(1-a**2/seuil**2)
-        seuil_b = min(math.ceil(seuil*coef_b),seuil-1)
+        seuil_b = min(math.ceil(math.sqrt(seuil**2-a**2)),seuil-1)
         for b in range(a+1,seuil_b+1):
             test = math.sqrt(a**2+b**2)
-            seuil_bas = math.floor(test)
-            seuil_haut = math.ceil(test)
-            if seuil_bas<=seuil:
-                for c in range(max(max(a,b)+1,math.floor(test)),min(seuil,math.ceil(test))+1):
+            seuil_c_bas = max(b+1,math.floor(test))
+            seuil_c_haut = min(math.ceil(test),seuil)
+            if seuil_c_bas<=seuil:
+                for c in range(seuil_c_bas,seuil_c_haut+1):
                     if verification_triplet(a,b,c):
                         resultat.append((a,b,c))
     return resultat
