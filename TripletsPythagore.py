b6d7fbae3b25eb039c34d307e1ca46c37696b53c
jshederer <55618151+jshederer@users.noreply.github.com>
2019-10-24 22:42:35 +0200

    0.12

---
+++ TripletsPythagore.py
@@ -0,0 +1,18 @@
+﻿import math
+
+def liste_triplets_optimise(seuil):
+    resultat = []
+    coef_a = (-1+math.sqrt(2*seuil**2-1))/(2*seuil)
+    seuil_a = math.ceil(seuil*coef_a)
+    for a in range(1,seuil_a+1):
+        coef_b = math.sqrt(1-a**2/seuil**2)
+        seuil_b = min(math.ceil(seuil*coef_b),seuil-1)
+        for b in range(a+1,seuil_b+1):
+            test = math.sqrt(a**2+b**2)
+            seuil_bas = math.floor(test)
+            seuil_haut = math.ceil(test)
+            if seuil_bas<=seuil:
+                for c in range(max(max(a,b)+1,math.floor(test)),min(seuil,math.ceil(test))+1):
+                    if verification_triplet(a,b,c):
+                        resultat.append((a,b,c))
+    return resultat
