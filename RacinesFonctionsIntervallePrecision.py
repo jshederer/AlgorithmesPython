e9f42f623bced460c772d48a5f0dc8ae10056a85
hedererjs <hedererjs@asperience.fr>
2024-10-20 19:21:25 +0200

    Mise à jour algorithme de recherche de racines de polynôme

--- RacinesFonctionsIntervallePrecision.py
+++ RacinesFonctionsIntervallePrecision.py
@@ -1,9 +1,9 @@
-﻿
+﻿from math import *
 def test1(x):
-    return x**3-3*x+4
+    return (x)**3-3*(x)+4
 
 def test2(x):
-    return x**2-3*x-4
+    return (x)**2-3*(x)-4
 
 def test3(x):
     return x**2-3*x+4
@@ -11,6 +11,10 @@ def test3(x):
 test4="x**3-3*x+4"
 test5="x**2-3*x-4"
 test6="x**2-3*x+4"
+test7="3*x**5-2*x**4+2*x**2-4*x+1"
+
+def test8(x):
+    return 3*(x)**5-2*(x)**4+2*(x)**2-4*(x)+1
 
 def transformation_fonction(fonction,x):
     if type(fonction) == type(str()):
@@ -22,28 +26,38 @@ def trouver_racines(fonction,min,max,precision):
     if min>=max:
         print("Le minimum de recherche doit être inférieur au maximum de recherche")
         return False
-    x=min
-    xprec = x
-    fprec = transformation_fonction(fonction,x)
+    if type(fonction) == type(str()):
+        fonction = fonction.replace("x","(x)")
+    x=xprec=min
     pas = 10**(-precision)
     print("Pas de balayage de x pour la recherche de racine(s):",pas)
+    fprec = transformation_fonction(fonction,x)
     trouve = False
-    while x<=max:
-        x=x+pas
+    compteur = 0
+    while x<max:
         fx = transformation_fonction(fonction,x)
         if fx*fprec<=0:
-            #print("Bascule: image de ",xprec,"=>",fprec,"/image de ",x,"=>",fx)
             trouve = True
-            if abs(fprec) < abs(fx):
-                print("Racine de la fonction:",round(xprec,precision))
+            compteur += 1
+            xprec_precision=round(xprec,precision)
+            x_precision=round(x,precision)
+            if transformation_fonction(fonction,xprec_precision)==0:
+                print("Racine de la fonction: %s"%(xprec_precision))
+            elif transformation_fonction(fonction,x_precision)==0:
+                print("Racine de la fonction: %s"%(x_precision))
             else:
-                print("Racine de la fonction:",round(x,precision))
+                print("Racine de la fonction entre %s et %s"%(xprec_precision,x_precision))
+
         xprec=x
         fprec=fx
+        x=x+pas
+        
     if not trouve:
         print("Pas de racine pour la fonction")
         return False
     else:
+        if compteur > 1:
+            print("%d racines au total"%(compteur))
         return True
 
 def trouver_racines_param(fonction,min,max,precision):
@@ -72,4 +86,6 @@ def trouver_racines_input():
     # trouver_racines_param(test4,-3,-2,5)
     # trouver_racines_param(test5,-20,20,4)
     # trouver_racines_param(test6,-20,20,4)
+    # trouver_racines_param(test7,-2,2,4)
+    # trouver_racines_param(test8,-2,2,7)
     # trouver_racines_input()
